c9f3f4596b38e8f03f1211c038c7556e4122f867
bsmile32 <mr.batt.smile@gmail.com>
2015-02-16 23:11:37 +0700

    test2

--- VloveImport/VloveImport.data/TransactionData.cs
+++ VloveImport/VloveImport.data/TransactionData.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace VloveImport.data
 {
-    class TransactionData
+    public class TransactionData : CustomerData
     {
+
+        public int TRAN_ID { get; set; }
+        public int TRAN_TYPE { get; set; }
+        public int TRAN_TABLE_TYPE { get; set; }
+        public string TRAN_DETAIL { get; set; }
+        public string TRAN_REMARK { get; set; }
+        public Nullable<DateTime> TRAN_DATE { get; set; }
+        public double TRAN_AMOUNT { get; set; }
+        public int TRAN_STATUS { get; set; }
+        public int TRAN_STATUS_APPROVE { get; set; }
+        public int EMP_ID_APPROVE { get; set; }
+        public Nullable<DateTime> EMP_APPROVE_DATE { get; set; }
+        public string EMP_REMARK { get; set; }
+        public int ORDER_ID { get; set; }
+        public int PAYMENT_TYPE { get; set; }
+        public Nullable<DateTime> PAYMENT_DATE { get; set; }
+        public string PAYMENT_TIME { get; set; }
+
     }
 }
