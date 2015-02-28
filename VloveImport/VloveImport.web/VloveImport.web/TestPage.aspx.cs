09fe7f666621546de5cb20c101afcf529b8357f9
Bommy <rusjruth@gmail.com>
2015-02-28 17:30:15 +0700

    เพิ่มหน้า "TestPage" ขึ้นมา ครับ ลองเอาไปเล่นๆดู

---
+++ VloveImport/VloveImport.web/VloveImport.web/TestPage.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VloveImport.web
+{
+    public partial class TestPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [WebMethod]
+        //public static string SampleMethod(object txt)
+        public static string SampleMethod(string cb, string rdb, string dtMat, string dtHTML, string ddl)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            
+            //do process
+
+            return js.Serialize("");
+        }
+    }
+}
