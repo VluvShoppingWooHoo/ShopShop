74fd9009f7f86ddf9c7a838920a5d3a06e014c55
Smartike <striker999999999@gmail.com>
2015-03-01 03:40:40 +0700

    add shopping

---
+++ VloveImport/VloveImport.data/ShoppingData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VloveImport.data
+{
+    public class ShoppingData : CommonData
+    {
+        public int CUS_ID { get; set; }
+        public int PROD_ID { get; set; }
+        public int CUS_BK_NUMBER { get; set; }
+        public int CUS_BK_AMOUNT { get; set; }
+        public float CUS_BK_PRICE { get; set; }
+        public string CUS_BK_REMARK { get; set; }
+        public string CUS_BK_URL { get; set; }
+
+    }
+}
