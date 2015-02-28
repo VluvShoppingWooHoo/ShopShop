f7a091854fee0287b23ed59570b59b2ef366f816
Smartike <striker999999999@gmail.com>
2015-03-01 03:41:27 +0700

    add shopping

--- VloveImport/VloveImport.web/VloveImport.web/UserControls/ucSeachBox.ascx.cs
+++ VloveImport/VloveImport.web/VloveImport.web/UserControls/ucSeachBox.ascx.cs
@@ -7,6 +7,7 @@ using System.Web.Script.Services;
 using System.Web.Services;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using VloveImport.biz;
 using VloveImport.data;
 
 namespace VloveImport.web.UserControls
@@ -20,7 +21,35 @@ namespace VloveImport.web.UserControls
 
         public static void AddToCart()
         {
+            ucSeachBox box = new ucSeachBox();
+            box.NewItem();
+
+            ShoppingBiz Biz = new ShoppingBiz();            
+            ShoppingData Data = box.GetData();
+            string Result = Biz.AddtoCart(Data);
+            if (Result == "")
+            {
+                //Close Popup
+            }
+            else
+            {
+                //Write Log error message
+            }
+                
+        }
+
+        protected void NewItem()
+        {
+            ShoppingBiz Biz = new ShoppingBiz();
 
         }
+
+        protected ShoppingData GetData()
+        {
+            ShoppingData Data = new ShoppingData();            
+            return Data;
+        }
+
+
     }
 }
