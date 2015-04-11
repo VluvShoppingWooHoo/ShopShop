97f6fc73176b5bccfd740d9eb57ad1cf0efdbb95
Bommy <rusjruth@gmail.com>
2015-04-11 13:02:40 +0700

    ..

--- VloveImport/VloveImport.web/VloveImport.web/Index.aspx.cs
+++ VloveImport/VloveImport.web/VloveImport.web/Index.aspx.cs
@@ -18,14 +18,10 @@ using System.Net;
 using System.Text;
 using System.IO;
 using VloveImport.web.App_Code;
-<<<<<<< HEAD
 //using SimpleBrowser;
 //using OpenQA.Selenium.PhantomJS;
 //using OpenQA.Selenium;
 
-=======
- 
->>>>>>> origin/master
 namespace VloveImport.web
 {
     public partial class Index : System.Web.UI.Page
